5447ab644bf1a547aa1cb5ecb00bf3ae493cb645
Ron Rademaker <ron@connectholland.nl>
2016-02-12 16:09:36 +0100

    Added basic structure

---
+++ src/Command/ReleaseCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace RonRademaker\ReleaseBuilder\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Command to create releases in Github
+ * Allows setting the release version as a constant in a class
+ *
+ * @author Ron Rademaker
+ */
+class ReleaseCommand extends Command
+{
+    /**
+     * The current version
+     */
+    const VERSION = '0.1-dev';
+
+    /**
+     * Configure the command
+     */
+    protected function configure()
+    {
+        $this->setName('release:build');
+    }
+
+    /**
+     * Create the release in github
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+    }
+}
