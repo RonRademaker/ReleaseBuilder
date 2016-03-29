4127227526644dca9831741a3adde5ee346885d9
Ron Rademaker <ron@connectholland.nl>
2016-03-29 17:17:18 +0200

    Updated version number for development

--- src/Command/ReleaseCommand.php
+++ src/Command/ReleaseCommand.php
@@ -25,7 +25,7 @@ class ReleaseCommand extends Command
     /**
      * The current version
      */
-    const VERSION = '7.0.0';
+    const VERSION = '7.0-dev';
 
     /**
      * Vendor or the repo to release
